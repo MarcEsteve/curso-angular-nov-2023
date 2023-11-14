11b6e5d31254e551564bfc8389b274ef92cafcaf
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2023-11-14 11:10:19 +0100

    Primeros componentes

--- angularnov2023/src/app/app.component.ts
+++ angularnov2023/src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularnov2023';
+  title = 'Angular Nov';
+  fecha = 2023;
+  mensaje:any = "Hola mundo";
 }
