a0852d3610541f3fd1f30192e84bb7d78b04991a
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2023-11-14 13:07:53 +0100

    data binding Property

--- angularnov2023/src/app/app.component.ts
+++ angularnov2023/src/app/app.component.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent, DataBindingComponent, PropertyBindingComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- angularnov2023/src/app/data-binding/data-binding.component.ts
+++ angularnov2023/src/app/data-binding/data-binding.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-data-binding',
+  standalone: true,
   templateUrl: './data-binding.component.html',
   styleUrls: ['./data-binding.component.css']
 })
