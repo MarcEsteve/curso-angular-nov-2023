b16d2e4e0790cfb77439f93e66d69d9ffa896533
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2023-11-15 10:28:21 +0100

    cambios

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
+  standalone:true,
+  imports: [BrowserModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
