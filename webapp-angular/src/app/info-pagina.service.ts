5c44bc84d07bab8c6612bf4caf23a0a6d1ea13a0
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2023-11-16 13:34:34 +0100

    Create info-pagina.service.ts

---
+++ webapp-angular/src/app/info-pagina.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InfoPaginaService {
+  
+  info: any = {};
+
+  constructor(private http: HttpClient) {
+    // console.log("Servicio de infoPagina listo")
+
+    // Leer el archivo JSON
+    this.http
+    .get('assets/data/data-pagina.json')
+    .subscribe((resp: any) => {
+      this.info = resp; // provar resp. I veurem les propietats JSON
+      console.log(resp);
+    });
+  }
+}
