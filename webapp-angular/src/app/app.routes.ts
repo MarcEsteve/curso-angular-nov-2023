fb0142a0842ac3c0813d563ca777a14d7dd704f9
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2023-11-16 13:18:06 +0100

    terminos ejmplo rutas

--- webapp-angular/src/app/app.routes.ts
+++ webapp-angular/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 import { Pagina404Component } from './pagina-404/pagina-404.component';
+import { TerminosComponent } from './pages/terminos/terminos.component';
 
 export const routes: Routes = [
     {path: 'about', component: AboutComponent},
     {path: 'portfolio', component: PortfolioComponent},
+    {path: 'terminos', component: TerminosComponent},
     {path: '', redirectTo: 'portfolio', pathMatch: 'full'},
     {path: '**', component: Pagina404Component}
 ];
--- webapp-angular/src/app/pagina-404/pagina-404.component.ts
+++ webapp-angular/src/app/pagina-404/pagina-404.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pagina-404',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './pagina-404.component.html',
   styleUrl: './pagina-404.component.css'
 })
